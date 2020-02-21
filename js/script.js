e1e77760333670e7fe957b354fc9c386e9ab6f38
Martin Kanestrøm <martinkanestrom95@gmail.com>
2020-02-21 10:19:23 +0100

    Updated index.html to show all characters and then hide loader;

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// https://rickandmortyapi.com/api/character/
+
+let characterQuery = fetch("https://rickandmortyapi.com/api/character/")
+    .then(response => {
+        return response.json();
+    })
+    .then(json => {
+        console.log(json.results);
+        console.log(json.results[0].name);
+        createDetailsList(json.results);
+        removeLoader();
+    })
+    .catch(error => {
+        window.location.replace("error.html");
+    })
+
+/*
+<div class="col-sm-6 col-md-4 col-lg-3">                
+    <div class="card">
+                   
+        <img class="image" src="https://via.placeholder.com/300" alt="Character Name">
+        <div class="details">
+            <h4 class="name">Character Name</h4>
+            <p>Type: Type value here</p>    
+            <p>Episode count: Episode count here</p>                                       
+            <a class="btn btn-primary" href="details.html?id=">Details</a>
+        </div>
+    </div>
+</div>
+
+*/
+
+function removeLoader() {
+    document.querySelector(".loader").style.display = "none";
+}
+
+function createDetailsList(characterDetail) {
+    const resultsRow = document.querySelector("div.row.results");
+    characterDetail.forEach(element => {
+        let type = "";
+        if(element.type === null || element.type === undefined) {
+            type = "Unknown";
+        } else {
+            type = element.type;
+        }
+        resultsRow.innerHTML += `<div class="col-sm-6 col-md-4 col-lg-3">                
+                                <div class="card">
+                                            
+                                    <img class="image" src="${element.image}" alt="Character Name">
+                                    <div class="details">
+                                        <h4 class="name">${element.name}</h4>
+                                        <p>Type: ${type}</p>    
+                                        <p>Episode count: ${element.episode.length}</p>                                       
+                                        <a class="btn btn-primary" href="details.html?id=${element.id}">Details</a>
+                                    </div>
+                                </div>
+                            </div>`
+    });
+    
+}
